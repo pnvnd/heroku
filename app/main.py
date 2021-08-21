239fb6081a978f703c388083c28aa127b3e6bfea
pnvnd <peter@datacrunch.ca>
2021-08-21 09:15:58 -0400

    test

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 
 # Flask Web Application
-app = Flask("main")
+app = Flask(__name__)
 
 # Navigation
 @app.route("/")
@@ -9,4 +9,5 @@ def index():
     return render_template("index.html")
 
 # Run Flask Web Application
-app.run()
+if __name__ == "__main__":
+    app.run()
